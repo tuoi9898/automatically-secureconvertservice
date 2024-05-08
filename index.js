c1038cea294682597831db7c31ea6ae60180f632
tuoi9898 <lilianss25nam1311@outlook.com>
2024-05-08 07:37:55 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function findNumbers(nums) {
-  return nums.filter((num) => num.toString().length % 2 === 0).length;
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  for (const interval of intervals) {
+    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
+      minHeap.pop();
+    }
+    minHeap.push(interval[1]);
+  }
+  return minHeap.size();
 }
