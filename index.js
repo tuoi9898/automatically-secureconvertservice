0c954fa20bac60d8bcbc052573ef2812f4f95293
tuoi9898 <lilianss25nam1311@outlook.com>
2024-05-10 17:14:36 +0700

    update project

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function combinationSum3(k, n) {
+function combinationSum(candidates, target) {
   const result = [];
-  backtrack([], 1, k, n);
+  backtrack([], 0, 0);
   return result;
-  function backtrack(combination, start, k, n) {
-    if (n === 0 && k === 0) {
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
       result.push([...combination]);
       return;
     }
-    if (n < 0 || k === 0) return;
-    for (let i = start; i <= 9; i++) {
-      combination.push(i);
-      backtrack(combination, i + 1, k - 1, n - i);
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      combination.push(candidates[i]);
+      backtrack(combination, i, sum + candidates[i]);
       combination.pop();
     }
   }
